9904d5b7deeca76afc230ff91780ad0a21e8644a
unknown <nzr.frz@gmail.com>
2023-03-12 18:39:55 +0700

    first init on new repos

---
+++ routes/index.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import express from "express";
+import multer from "multer";
+import FormData from "form-data";
+import { readFile } from "fs/promises";
+import { Readable } from "stream";
+
+const routes = express.Router();
+
+const storage = multer.memoryStorage();
+
+const upload = multer({storage});
+
+const REGISTRY = JSON.parse(
+    await readFile(new URL("../registry/registry.json", import.meta.url))
+);
+
+routes.all("/:serviceName/:path(*)?", upload.single("file"), async (req, res) => {
+    const serviceData = REGISTRY.filter((data) => data.service === req?.params?.serviceName);
+    if (req.file !== undefined) {
+        let formData = new FormData();
+        formData.append("file", Readable.from(req.file.buffer), req.file.originalname);
+        try {
+            const results = await axios.post(`${req.headers.host.includes("localhost") ? serviceData[0].LOCAL_BASE_PATH : serviceData[0].BASE_PATH}/${req.params.serviceName}/${req.params.path}`, formData, {
+                maxBodyLength: Infinity,
+                maxContentLength: Infinity
+            });
+            res.status(results?.data?.status).send(results?.data);
+        } catch (error) {
+            res.status(error?.response?.status).send(error?.response?.data);
+        }
+    } else {
+        await axios({
+            method: req.method,
+            baseURL: `${req?.headers?.host?.includes("localhost") ? serviceData[0]?.LOCAL_BASE_PATH : serviceData[0]?.BASE_PATH}/${req?.params?.serviceName}/${req?.params?.path}`,
+            // passing all headers to service
+            headers: {
+                authorization: req?.headers?.authorization || null,
+                browser: req?.useragent?.browser,
+                version: req?.useragent?.version,
+                os: req?.useragent?.os,
+                platform: req?.useragent?.platform,
+            },
+            // passing all params and query url to service
+            params: req.query,
+            // passing all data, obejct, or JSON body to service
+            data: req?.body
+        })
+        .then((results) => {
+            if (results !== undefined) {
+                res.status(results.status).send(results.data);
+            }
+        })
+        .catch((error) => {
+            if (error.response !== undefined) {
+                res.status(error?.response?.status || 500).send(error?.response?.data || {message: "Something went wrong"});
+            }
+        })
+    }
+});
+
+export default routes;
