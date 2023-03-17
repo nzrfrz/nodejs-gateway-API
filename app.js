67b1693feb511681a21e6d1705149fd49a920828
unknown <nzr.frz@gmail.com>
2023-03-17 11:54:11 +0700

    adding more cors options list

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 let corsOptions = {
-    origin: "http://localhost:3000",
+    origin: corsOptionsList,
     credentials: true,
     optionsSuccessStatus: 200,
     exposedHeaders: ["set-cookie"],
